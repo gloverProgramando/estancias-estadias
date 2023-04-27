d1447524312d9c77c404a69c30a0f63b7db2ada9
Isaac Master <isaac_817221mayorix@hotmail.com>
2023-04-27 15:04:43 -0500

    Guia de Usuario
    Introduje el onboarding , que es la guia de usuario y un alert

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,11 +6,13 @@
     @include('plantilla/alumno/navBar')
 
     <div class="container-fluid colorbg">
-
+    <button class="onboarding-btn">Guia rapida</button>
         <div class="titulo">
             <h2 class="estiloTitulo">Registros <small>(Estancias/Estadías)</small></h2>
+           
         </div>
         <div class="contenedor">
+        
             <div class="carta">
                 <h4>Estancias</h4>
                 <img src="{{ asset('/css/documentos.png') }}">
@@ -43,12 +45,88 @@
                 </div>
 
             </div>
+           <div>
+           <div class="div" ></div>
+           </div>
+           
             <div class="estiloAviso">
 				<embed src="css/calendario.pdf#toolbar=0" height="500" width="1000" type="" style="padding: 2%">
             </div>
+            
         </div>
+
+        <div>
+        <div class="onboarding-overlay" ></div>
+        <div class="onboarding-container">
+            <div class="content">
+                <a href="/views/inicio.blade.php" class="skip-btn">Skip</a>
+                <div class="dots">
+                    <div class="dot active"></div>
+                    <div class="dot"></div>
+                    <div class="dot"></div>
+                </div>
+<!--Paso 1 -->
+                <div class="steps">
+                    <div class="step">
+                        <div class="image">
+                        <img src="{{ asset('/css/Estan1y2.png') }}">
+                        </div> 
+                    
+                        <h3>Paso 1</h3>
+                        <p>
+                       <!--Si le quieres añadir algun mensaje debajo de 'paso#' ya esta configurado para que lo
+                        puedas añadir aqui adentro -->   
+                        </p>
+                    </div>
+                    <div class="step">
+                        <div class="image">
+                        <img src="{{ asset('/css/Step2.png') }}">
+                        </div> 
+                
+                        <h3>Paso 2</h3>
+                        <p>
+                            <!--Si le quieres añadir algun mensaje debajo de 'paso#' ya esta configurado para que lo
+                        puedas añadir aqui adentro -->
+                        </p>
+                    </div>
+                    <div class="step">
+                        <div class="image">
+                        <img src="{{ asset('/css/guiaPDF.png') }}">
+                        </div> 
+                        <p>
+                       <!--Si le quieres añadir algun mensaje debajo de 'paso#' ya esta configurado para que lo
+                        puedas añadir aqui adentro --> 
+                        <h3><a href="/css/GUIADEUSO.pdf">Descarga aqui</a></h3>  
+                        </p>
+                    </div>
+                </div>
+<!--Paso 2 -->
+                <button class="next-btn" >Siguiente</button>
+            </div>
+            <script src="{{ asset('/js/main2.js') }}"></script>
+        </div>
+		</div>
     </div>
     @include('plantilla/alumno/footer')
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script>
+    window.onload = function() {
+    var showAlert = true;
+    function showSweetAlert() {
+        if (showAlert) {
+            showAlert = false;
+            swal({
+                title: "¡AVISO!",
+                text: "¡RECUERDA SUBIR TUS DOCUMENTOS EN TIEMPO Y FORMA!",
+                icon: "warning",
+                button: "Ok",
+            }).then(function() {
+                swal.close();
+            });
+        }
+    }
+    showSweetAlert();
+}
+</script>
 </body>
-
 </html>
--- resources/views/plantilla/alumno/head.blade.php
+++ resources/views/plantilla/alumno/head.blade.php
@@ -8,7 +8,8 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    
+   <!--guia de usuario css-->
+    <link rel="stylesheet" href='{{ asset("/css/welcomeguia.css") }}'>
 
     <script src="https://www.google.com/recaptcha/api.js"></script>
 
